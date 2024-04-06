e30b5bef7cab8a7c2e9c444141d279f8c8646663
minute <lukas@mntre.com>
2024-04-06 13:09:00 +0000

    firefox syspref.js: remove ads on new tab page

--- etc/firefox/syspref.js
+++ etc/firefox/syspref.js
@@ -1,2 +1,8 @@
+// performance tweaks
 pref("layers.acceleration.force-enabled", true);
 pref("general.smoothScroll", false);
+
+// remove ads
+pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+pref("browser.topsites.contile.enabled", false);
