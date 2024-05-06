1980e5d1cab5d0347465aef92490a4708b537c61
Lukas F. Hartmann <lukas@mntre.com>
2024-05-06 13:17:03 +0200

    etc/profile.d/reform.sh: remove harmful QT_QPA_PLATFORM and outdated ETNA_MESA_DEBUG
     - ETNA_MESA_DEBUG=nir is no longer needed because nir is the default
     - QT_QPA_PLATFORM=wayland breaks some qt apps if wayland backend is not
       installed (qt6-wayland or qtwayland5)

--- etc/profile.d/reform.sh
+++ etc/profile.d/reform.sh
@@ -1,14 +1,7 @@
 # Defaults for MNT Reform
 
-# enable NIR shader path in mesa. without this,
-# some Xwayland applications will be blank
-export ETNA_MESA_DEBUG=nir
-
 # set GTK2 theme
 export GTK2_RC_FILES=/usr/share/themes/Adwaita-Dark/gtk-2.0/gtkrc
-
-# set QT Platform
-export QT_QPA_PLATFORM=wayland
 
 # enable harware acceleration in clapper
 # https://github.com/Rafostar/clapper/wiki/Hardware-acceleration#mobile-devices-v4l2-codecs
