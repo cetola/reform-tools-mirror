709d77cb8e49596b9939ccd7ab3f1eca0fd85b90
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2024-08-01 04:16:08 +0200

    stop shipping files in /etc
     - stop pretending we can update them by changing them in reform-tools
     - their initial versions will get shipped in the system image
     - this also corrects the date in motd to the creation date of the image

--- etc/firefox/syspref.js
+++
@@ -1,8 +0,0 @@
-// performance tweaks
-pref("layers.acceleration.force-enabled", true);
-pref("general.smoothScroll", false);
-
-// remove ads
-pref("browser.newtabpage.activity-stream.feeds.topsites", false);
-pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
-pref("browser.topsites.contile.enabled", false);
