6f5d51c87cecd1dc7cb88dfc971c33b5457e6236
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2025-11-18 18:01:50 +0100

    etc/profile.d/reform-mali.sh: set PAN_MESA_DEBUG=gl3
    PAN_MESA_DEBUG=gl3 enables OpenGL 3.3 from 3.1 (but doesn't change
    OpenGL ES) on Mali GPUs (like in RK3588 and A311D) by skipping over some
    features that normally don't allow the driver to advertise these
    versions, making some applications and games work that don't work with
    older OpenGL versions.

---
+++ etc/profile.d/reform-mali.sh
@@ -0,0 +1,5 @@
+# PAN_MESA_DEBUG=gl3 enables OpenGL 3.3 from 3.1 (but doesn't change OpenGL ES)
+# on Mali GPUs (like in RK3588 and A311D) by skipping over some features that
+# normally don't allow the driver to advertise these versions, making some
+# applications and games work that don't work with older OpenGL versions.
+PAN_MESA_DEBUG=gl3
