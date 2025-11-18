9efd2aae19730bd598689d400533c828d1357fa0
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2025-11-18 18:01:21 +0100

    etc/profile.d/reform-kwin.sh: expand comment

--- etc/profile.d/reform-kwin.sh
+++ etc/profile.d/reform-kwin.sh
@@ -1,3 +1,6 @@
-# TODO: The bug for which the below is a workaround is yet to be filed with upstream.
-#       See http://mntre.com/reform-irc-logs/2025-09-15.log.html#t17:48:39
+# KWIN_FORCE_SW_CURSOR=1 is currently needed because the RK3588 VOP2 driver
+# doesn't report or enforce the limitations for overlays on certain displays,
+# resulting in a black square behind the cursor and the display pipeline
+# crashing when the cursor is moved into the screen edges.
+# The bug is wontfix upstream. TODO: find the bug URL
 KWIN_FORCE_SW_CURSOR=1
